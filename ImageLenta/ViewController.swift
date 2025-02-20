cc2a82cb437f91b9055e8f98db0bcde36682e957
DiK <korolevadidan@gmail.com>
2025-02-20 20:39:46 +0700

    Initial Commit

---
+++ ImageLenta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLenta
+//
+//  Created by Ди Di on 20/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImageLentaTests/ImageLentaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLentaTests.swift
+//  ImageLentaTests
+//
+//  Created by Ди Di on 20/02/25.
+//
+
+import Testing
+@testable import ImageLenta
+
+struct ImageLentaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
